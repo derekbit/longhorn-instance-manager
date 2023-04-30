23aa9fd8f75db5d3bb46a08073a7bb3938ec5774
Derek Su <derek.su@suse.com>
2023-04-30 21:20:15 +0800

    spdk: add disk, instance, proxy, and spdk gRPC services in generate_grpc.sh
    Signed-off-by: Derek Su <derek.su@suse.com>

--- generate_grpc.sh
+++ generate_grpc.sh
@@ -18,10 +18,26 @@ if [ ! -e ${DIR} ]; then
     wget https://raw.githubusercontent.com/longhorn/longhorn-engine/master/proto/ptypes/syncagent.proto -P $DIR
 fi
 
-# instance manager
+# common
+python3 -m grpc_tools.protoc -I pkg/imrpc -I proto/vendor/protobuf/src/ --python_out=integration/rpc/common --grpc_python_out=integration/rpc/common pkg/imrpc/imcommon.proto
+protoc -I pkg/imrpc/ -I proto/vendor/protobuf/src/ pkg/imrpc/imcommon.proto --go_out=plugins=grpc:pkg/imrpc
+
+# instance manager (process manager)
 python3 -m grpc_tools.protoc -I pkg/imrpc -I proto/vendor/protobuf/src/ --python_out=integration/rpc/instance_manager --grpc_python_out=integration/rpc/instance_manager pkg/imrpc/imrpc.proto
 protoc -I pkg/imrpc/ -I proto/vendor/protobuf/src/ pkg/imrpc/imrpc.proto --go_out=plugins=grpc:pkg/imrpc
 
 # proxy
 python3 -m grpc_tools.protoc -I pkg/imrpc -I proto/vendor/ -I proto/vendor/protobuf/src/ --python_out=integration/rpc/proxy --grpc_python_out=integration/rpc/proxy pkg/imrpc/proxy.proto
 protoc -I pkg/imrpc/ -I proto/vendor/ -I proto/vendor/protobuf/src/ pkg/imrpc/proxy.proto --go_out=plugins=grpc:pkg/imrpc/
+
+# disk
+python3 -m grpc_tools.protoc -I pkg/imrpc -I proto/vendor/ -I proto/vendor/protobuf/src/ --python_out=integration/rpc/disk --grpc_python_out=integration/rpc/disk pkg/imrpc/disk.proto
+protoc -I pkg/imrpc/ -I proto/vendor/ -I proto/vendor/protobuf/src/ pkg/imrpc/disk.proto --go_out=plugins=grpc:pkg/imrpc/
+
+# instance
+python3 -m grpc_tools.protoc -I pkg/imrpc -I proto/vendor/ -I proto/vendor/protobuf/src/ --python_out=integration/rpc/instance --grpc_python_out=integration/rpc/instance pkg/imrpc/instance.proto
+protoc -I pkg/imrpc/ -I proto/vendor/ -I proto/vendor/protobuf/src/ pkg/imrpc/instance.proto --go_out=plugins=grpc:pkg/imrpc/
+
+# spdk
+python3 -m grpc_tools.protoc -I pkg/imrpc -I proto/vendor/ -I proto/vendor/protobuf/src/ --python_out=integration/rpc/spdk --grpc_python_out=integration/rpc/spdk pkg/imrpc/spdk.proto
+protoc -I pkg/imrpc/ -I proto/vendor/ -I proto/vendor/protobuf/src/ pkg/imrpc/spdk.proto --go_out=plugins=grpc:pkg/imrpc/
